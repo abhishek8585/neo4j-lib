e5a7a08e7986caf8ff3248db4de5b9661ceda935
Venkatesh Tadinada <vtadinad@bmc.com>
2022-02-03 14:23:26 -0800

    first version of tools

--- neo_lib.py
+++ neo_lib.py
@@ -1,66 +1,75 @@
 
-from lib2to3.pgen2 import driver
-
-
-def run_cypher(cq,limit=10):
-    tran = lambda tx: tx.run(cq,limit=limit).data()
-    global driver
-    global neo_database
-    with driver.session(database=neo_database) as session:
-        results = session.write_transaction(tran)
-    return results
-
-def reset_db():
-    cq = "match (n) detach delete n"
-    return run_cypher(cq)
-
-def get_stats():
-    cq = """
-    call apoc.meta.stats() yield labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
-    with labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
-    return labelCount, relTypeCount,propertyKeyCount, nodeCount,relCount
-    """
-    res = run_cypher(cq)
-    for rec in res:
-        print (rec)
-
-def checksum():
-    cq = """
-    call apoc.meta.stats() yield labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
-    with labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
-    return labelCount+relTypeCount+propertyKeyCount+nodeCount+relCount as checksum
-    """
-    res = run_cypher(cq)
-    return res[0]['checksum']
-
-def nodeCount():
-    cq = """
-    match (n) return count(n) as count
-    """
-    res = run_cypher(cq)
-    return res[0]['count']
-
+from contextlib import nullcontext
+import pandas as pd
 from pprint import pprint
-def get_stats_all():
-    cq = """
-        call apoc.meta.stats()
+
+from neo4j import GraphDatabase, basic_auth
+from py2neo import Graph
+class neo_lib:
+
+    def login(self, neo_url, neo_user, neo_pwd, neo_database)):
+        self.driver = GraphDatabase.driver(neo_url, auth=basic_auth(neo_user, neo_pwd))
+        self.neo_database = neo_database
+        self.graph = Graph(neo_url, auth=(neo_user, neo_pwd))
+
+
+
+    def run_cypher(self,cq,limit=10):
+        tran = lambda tx: tx.run(cq,limit=limit).data()
+        with self.driver.session(database=self.neo_database) as session:
+            results = session.write_transaction(tran)
+        return results
+
+    def reset_db(self):
+        cq = "match (n) detach delete n"
+        return self.run_cypher(cq)
+
+    def get_stats(self):
+        cq = """
+        call apoc.meta.stats() yield labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
+        with labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
+        return labelCount, relTypeCount,propertyKeyCount, nodeCount,relCount
         """
-    res = run_cypher(cq)
-    pprint(res[0]['stats'])
+        res = self.run_cypher(cq)
+        for rec in res:
+            print (rec)
 
-def schema_view():
-    cq = "CALL db.schema.visualization()"
-    print ("Run {} in Neo4j Browser to see a graphical view".format(cq))
-    res  = run_cypher(cq)
-    pprint(res)
+    def checksum(self):
+        cq = """
+        call apoc.meta.stats() yield labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
+        with labelCount, relTypeCount, propertyKeyCount, nodeCount, relCount
+        return labelCount+relTypeCount+propertyKeyCount+nodeCount+relCount as checksum
+        """
+        res = self.run_cypher(cq)
+        return res[0]['checksum']
 
-def print_label_count():
-    result = {"Label": [], "Count": []}
-    for label in graph.run("CALL db.labels()").to_series():
-        query = f"MATCH (:`{label}`) RETURN count(*) AS count"
-        count = graph.run(query).to_data_frame().iloc[0]['count']
-        result["Label"].append(label)
-        result["Count"].append(count)
-    nodes_df = pd.DataFrame(data=result)
-    return nodes_df
+    def nodeCount(self):
+        cq = """
+        match (n) return count(n) as count
+        """
+        res = self.run_cypher(cq)
+        return res[0]['count']
+
+    def get_stats_all(self):
+        cq = """
+            call apoc.meta.stats()
+            """
+        res = self.run_cypher(cq)
+        pprint(res[0]['stats'])
+
+    def schema_view(self):
+        cq = "CALL db.schema.visualization()"
+        print ("Run {} in Neo4j Browser to see a graphical view".format(cq))
+        res  = self.run_cypher(cq)
+        pprint(res)
+
+    def print_label_count(self):
+        result = {"Label": [], "Count": []}
+        for label in self.graph.run("CALL db.labels()").to_series():
+            query = f"MATCH (:`{label}`) RETURN count(*) AS count"
+            count = self.graph.run(query).to_data_frame().iloc[0]['count']
+            result["Label"].append(label)
+            result["Count"].append(count)
+        nodes_df = pd.DataFrame(data=result)
+        return nodes_df
 
