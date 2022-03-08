792d5327ba98bdba4889b73f62addbe07f3525e3
Venkatesh Tadinada <vtadinad@bmc.com>
2022-03-08 09:03:32 +0530

    added parmaeters to run

--- neo_lib.py
+++ neo_lib.py
@@ -17,9 +17,9 @@ class Neo_lib:
 
 
 
-    def run_cypher(self,cq,limit=10):
+    def run_cypher(self,cq,parameters=None,limit=10):
         try:
-            tran = lambda tx: tx.run(cq,limit=limit).data()
+            tran = lambda tx: tx.run(cq,parameters=parameters,limit=limit).data()
             with self.driver.session(database=self.neo_database) as session:
                 results = session.write_transaction(tran)
         except Exception as e:
